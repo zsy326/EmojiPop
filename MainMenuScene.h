0bbb1084a2fa47c08a073ca004a04265ce8e1d74
zsy326 <57891166+zsy326@users.noreply.github.com>
2020-06-04 11:51:06 +0800

    Create MainMenuScene.h
    [MainMenu] conclude : goToGame, setBgm &  ExitGame

---
+++ MainMenuScene.h
@@ -0,0 +1,22 @@
+#ifndef __MAINMENU_SCENE_H__
+#define __MAINMENU_SCENE_H__
+
+#include "cocos2d.h"
+
+class MainMenu : public cocos2d::Scene
+{
+public:
+    static cocos2d::Scene* createScene();
+
+    virtual bool init();
+
+    void menuCloseCallback(cocos2d::Ref* pSender);
+    void GoToGameScene(cocos2d::Ref* pSender);
+    void GoToMusicScene(cocos2d::Ref* pSender);
+
+    CREATE_FUNC(MainMenu);
+};
+
+#endif
+
+
